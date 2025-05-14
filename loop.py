ba2cb8e12c540100892f4df9c3ff4a58fa47114e
pnamichaels <pnamichaels@gmail.com>
2025-05-14 09:15:01 -0500

    changed loop.py to reflect the assignment answer

--- loop.py
+++ loop.py
@@ -1,11 +1,10 @@
-my_list = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday",]
+my_list = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
 count = 1
 
 while count <= 3:
     for i in my_list:
         if i == "Monday":
-            print('We dont speak of this day')
-        else:
-            print(i)
-    
+            continue
+        print(i, "\n")
+    print("---------------------------------")
     count += 1
